3f5600763fe78e67e4bfa4714f93467e02e4d2f9
imsudhanshu09 <sudhanshu9jaiswal@gmail.com>
2024-04-01 22:26:02 +0530

    updates font writing

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -124,24 +124,27 @@ const Rules = () => {
       </div>
       <div>
         <ul className='u'>
-          <li><u>There are total 8 rules, listed below</u></li>
+          <li><u>There are total 9 rules, listed below</u></li>
         </ul>
         <ol className="u">
-          <li>OTH will begin at "06/04/24 at 00:00" and will end at "08/04/24 at 00:00"</li>
+          <li>The Online Treasure Hunt (OTH) will commence on "04/04/24 at 23:59" and conclude on "07/04/24 at 23:59".</li>
           <br />
-          <li>You are allowed to use internet and AI tools to search answer. If the answer is correct then only you can move on to the next question</li>
+          <li>You're permitted to utilize the internet and AI tools to find answers. Only when your response is accurate can you proceed to the next question.</li>
           <br />
-          <li>All answers should be in small letters. For example, if the answer is "Mumbai" then write "mumbai" as the answer</li>
+          <li>All answers should be in lowercase. For instance, if the answer is "Mumbai," then write "mumbai".</li>
           <br />
-          <li>No space should be given among the answer. For example, if the answer is <br/>"Rohit Sharma" then the answer should be written as "rohitsharma"</li>
+          <li>Avoid spacing in your answers. For example, if the answer is "Rohit Sharma," write "rohitsharma".</li>
           <br />
-          <li>Full names must be written. For example, if the answer is "gandhiji" then the full name "mohandaskaramchandragandhi" is the answer</li>
+          <li>Provide full names when necessary. For instance, if the answer is "gandhiji," then the full name "mohandaskaramchandragandhi" is required.</li>
           <br />
-          <li>Numeric answers should be in numbers. For example, if the answer is "Krishh3" then the answer should be "krishh3"</li>
+          <li>Numeric answers should be presented in numbers. For example, if the answer is "Krishh3," write "krishhthree".</li>
           <br />
-          <li>Only one question contains a fully numeric answer. Example "1234"</li>
+          <li>Only one question entails a fully numeric answer. For instance, "12345" should be written as "onetwothreefourfive".</li>
           <br />
-          <li>Hints are based on the difficulty of questions. So the number of hints is relative to questions.</li>
+          <li>The number of hints provided correlates with the difficulty of the questions.</li>
+          <br/>
+          <li>If you have any questions or need assistance, please don't hesitate to reach out to the organizing team: <br/> Sudhanshu Kumar: 9523788886
+          <br/> Garv Jauhari: 6307129301  <br/> We're here to help! </li>
 </ol>
 <div className="background"></div>
       </div>
